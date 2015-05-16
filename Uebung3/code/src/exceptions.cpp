7d0c374bd10a968df14a586b9214bc47c7dc3f75
Erik Griese <erik.griese@student.hpi.uni-potsdam.de>
2015-05-16 17:29:35 +0200

    Solved 3.2

--- Uebung3/code/src/exceptions.cpp
+++ Uebung3/code/src/exceptions.cpp
@@ -6,6 +6,7 @@
 #include <iomanip>
 #include <vector>
 #include <ctime>
+#include <stdexcept>
 
 // transforms a string to a date. Throws a logic_error if year is *not* between 2005 and 2015
 std::tm stringToTime(std::string date)
@@ -33,7 +34,36 @@ struct FormatException
 void parseLine(std::string line, int lineNum)
 {
     const std::string fieldNames[3] = { "Date", "Temperature", "Rainfall" };
+	const char delim = ';';
     
+	std::string token;
+	int index = 0;
+
+	std::istringstream iline(line);
+
+	while (std::getline(iline, token, delim)) {
+		try {
+			switch (index) {
+				case 0:
+					stringToTime(token);
+					break;
+				case 1:
+				case 2:
+					std::stof(token);
+					break;
+				default:
+					throw std::logic_error("Invalid Token index");	//e.g. caused by to much tokens in one line
+					break; //not necessarry
+			}
+		} catch (const std::exception & e) { //catch any logic or runtime error
+			FormatException ex;
+			ex.m_actLine = lineNum;
+			ex.m_actFields = fieldNames[index];
+			throw ex;
+		}
+
+		index++;
+	}
     // todo 3.2b: parse a given line, check dates by calling stringToTime, check temperature/rainfall by calling std::stof. Catch all exceptions thrown by these methods. If there have been any exceptions, aggregate all necessary information into an instance of FormatException and throw that instance.
     
 }
@@ -41,6 +71,21 @@ void parseLine(std::string line, int lineNum)
 // todo 3.2c..
 void writeOutFormatException(const FormatException & e)
 {
+	static const char* logfilePath = "exceptions.log";
+	std::ofstream log;
+	log.exceptions(~std::ofstream::goodbit);
+
+	try {
+		log.open(logfilePath, std::ios::out | std::ios::app);
+
+		log << "Format Exception in line " << e.m_actLine << ": ";
+		log << "Field '" << e.m_actFields << "' is incorrect" << std::endl;
+		
+		log.close();
+	} catch (const std::ios_base::failure & e) {
+		std::cerr << "Error logging format exception. File IO error." << std::endl;
+	}
+		
     // todo 3.2d: export information (i.e., line number + invalid data fields) about exception to a logfile.
     // todo 3.2d: catch ios_base::failure
 }
@@ -49,9 +94,35 @@ void checkData(std::string path)
 {
 	int validLines = 0;
     int invalidLines = 0;
+	int lineNum;
+
+	std::string line;
+
     std::ifstream file;
+	file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+	try {
+		file.open(path);
+
+		lineNum = 1;
+		while (!file.eof()) {
+			std::getline(file, line);
+			try {
+				parseLine(line, lineNum);
+				validLines++;	//this part is only reached if no exception is thrown --> valid line
+			} catch (const FormatException & e) {
+				writeOutFormatException(e);
+				invalidLines++;
+			}
+			
+			lineNum++;
+		}
+
+		file.close();
+	} catch (const std::ifstream::failure & e) {
+		std::cerr << "Error operating on file" << std::endl;
+	}
     
-    // todo 3.2a: open file + read each line + call parseLine function (catch ifstream::failure)
     // todo 3.2c: read each line + call parseLine function (catch FormatException) + count valid + invalid lines
     
     std::cout << "valid lines: " << validLines << " - invalid lines: " << invalidLines << std::endl;

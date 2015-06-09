2c47f022a583ba5bf625de4275a0b58df3ea8452
Erik Griese <erik.griese@student.hpi.uni-potsdam.de>
2015-06-10 01:26:04 +0200

    5.1c gemacht

--- Uebung5/code/iterators.cpp
+++ Uebung5/code/iterators.cpp
@@ -19,21 +19,89 @@ void printContainer(T& container)
 template<class T>
 void front_back_pairing(T& inContainer, T& outContainer)
 {
+	int size = inContainer.size();
+	int j=size-1;
+	int i=0;
+	int pos=0;
 
+	//std::vector<T> v;//{ inContainer.begin(), inContainer.end() };
+	//std::copy(inContainer.begin(), inContainer.end(), v.begin());
+	while(i<size){
+		for(auto elem : inContainer){
+			//std::cout << elem;
+			if((i%2)==0){	
+				if(pos*2 != i){pos ++; continue;}
+					outContainer.push_back(elem);
+			}
+			else{ //std::cout << j << " : " << pos << std::endl;
+				if(pos != j){pos++; continue;}
+					outContainer.push_back(elem);
+					j--;
+			}
+			pos++;
+		}	
+	i++;
+	//std::cout << i;
+	pos=0;
+	}
 }
 
 // ToDo 5.1b - Remove all duplicates from the given container. Do *not* use the []-operator.
 template<class T>
 void remove_duplicates(T& container)
 {
+	int i=0;
+	int count=0;
 
+	for(auto &elem: container){
+		for(auto &inner: container){
+			if(elem == inner){
+				count++;
+				if(count >= 2) container.erase(container.begin()+i);
+			}
+			i++;
+		}
+	count=0;
+	i=0;
+	}
 }
 
 // ToDo 5.1c - Expand the given container by inserting the numerical differences of each element to its neighbors. Do *not* use the []-operator.
 template<class T>
 void insert_differences(T& container)
 {
+	#if 0
+	std::vector<T*>vec;
+	std::copy(container.begin(),container.end(),vec.begin());
 
+	int size = container.size();
+	int i=0;
+	int pos=0;
+
+	for(auto elem: container){
+		i++;
+	}
+	#endif
+	T tmpVec;	//create temporary vector for insertion
+	for (auto it=container.begin(); it!=container.end(); it++) {
+		//get neighbor positions
+		auto leftNeighbor = (it != container.begin()) ? it-1 : container.end()-1;
+		auto rightNeighbor = (it != (container.end()-1) ) ? it+1 : container.begin();
+
+		//calc differences
+		auto leftVal = *leftNeighbor - *it;
+		auto rightVal = *rightNeighbor - *it;
+
+		//do insertion
+		tmpVec.push_back(leftVal);
+		tmpVec.push_back(*it);
+		tmpVec.push_back(rightVal);
+	}
+
+	//copy tmpVec into to transforming vec
+	//first resize container to correct size
+	container.resize(tmpVec.size());
+	std::copy(tmpVec.begin(), tmpVec.end(), container.begin());
 }
 
 void testFrontBackPairingFunctionality()
@@ -80,7 +148,7 @@ void testAddDifferenceFunctionality()
     
     // test for integer vector
     printContainer(sampleDataInt);
-    add_differences(sampleDataInt);
+    insert_differences(sampleDataInt);
     printContainer(sampleDataInt);
 }
 

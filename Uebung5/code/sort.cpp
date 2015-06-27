a897d4b33ee94e2ee08507702fa4d980162ace7e
Erik Griese <erik.griese@student.hpi.uni-potsdam.de>
2015-06-27 21:37:03 +0200

    Solved 6.1

--- Uebung5/code/sort.cpp
+++ Uebung5/code/sort.cpp
@@ -21,7 +21,30 @@ void merge(T leftIt, T midIt, T rightIt)
 {
 	assert(leftIt <= midIt && midIt <= rightIt);
 
-	std::vector<typename T::value_type> mergedValues(rightIt - leftIt);
+	std::vector<typename T::value_type> mergedValues;
+
+	auto rightStartIt = midIt;
+	while ( (leftIt < midIt) && (rightIt > rightStartIt) ) {
+		if (*leftIt < *rightStartIt) {
+			mergedValues.push_back(*leftIt);
+			leftIt++;
+		} else {
+			mergedValues.push_back(*rightStartIt);
+			rightStartIt++;
+		}
+	}
+	//insert rest of other vec
+	while (leftIt < midIt) {
+		mergedValues.push_back(*leftIt);
+		leftIt++;
+	}
+	while (rightIt > rightStartIt) {
+		mergedValues.push_back(*rightStartIt);
+		rightStartIt++;
+	}
+
+	//copy sorted vector back
+	std::copy(mergedValues.begin(), mergedValues.end(), leftIt);
 }
 
 // ToDo 5.3 - Sort the given container using merge sort.
@@ -29,6 +52,18 @@ template<class T>
 void mergeSort(T leftIt, T rightIt)
 {
 	assert(leftIt < rightIt);
+	
+	if (std::distance(leftIt, rightIt) == 1) {
+		//do nothing if list only contains one element
+		return;
+	}
+
+	auto midIt = leftIt + std::distance(leftIt, rightIt)/2;
+
+	mergeSort(leftIt, midIt);
+	mergeSort(midIt, rightIt);
+
+	merge(leftIt, midIt, rightIt);
 }
 
 int main(int argc, char** argv)
